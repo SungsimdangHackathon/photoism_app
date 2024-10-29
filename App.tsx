fb9f427bd0b083efef40004e5237b4229ee0af68
taeyoooon <leteyune101010@gmail.com>
2024-10-29 20:19:24 +0900

    add:: 파일 구조

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
+
+function App(): React.JSX.Element {
+  return (
+    <SafeAreaView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+  },
+  text: {
+    fontSize: 20,
+  },
+});
+
+export default App;
